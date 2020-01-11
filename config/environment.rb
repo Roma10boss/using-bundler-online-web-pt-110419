1ca18949e98c4c8c4f99d05e566407827386fb50
Romario Gustave <romagustave10@gmail.com>
2020-01-11 00:14:33 +0000

    Automatically backed up by Learn

--- config/environment.rb
+++ config/environment.rb
@@ -5,7 +5,7 @@ source "https://rubygems.org"
 gem "rspec"
 gem "sinatra" , '1.4.4'
 gem "hashie"
-gem "octokit", '~>2.0'
+gem "octokit", '~> 2.0'
 gem "awesome_print", git: "git@github.com:michaeldv/awesome_print.git"
 gem "pry" , :group => :development
 
