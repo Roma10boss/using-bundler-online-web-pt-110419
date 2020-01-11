bc0d858509a90d8a5842a7bb1d69218a4eac80a6
Romario Gustave <romagustave10@gmail.com>
2020-01-11 00:15:40 +0000

    Automatically backed up by Learn

--- config/environment.rb
+++ config/environment.rb
@@ -2,11 +2,12 @@ require 'bundler/setup'
 Bundler.require(:default, :development)
 
 source "https://rubygems.org"
-gem "rspec"
+
 gem "sinatra" , '1.4.4'
 gem "hashie"
 gem "octokit", '~> 2.0'
 gem "awesome_print", git: "git@github.com:michaeldv/awesome_print.git"
+
 gem "pry" , :group => :development
 
 group "test" do
